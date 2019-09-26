6c8041761b8a86bd9c7970c254da28465fbbf6b9
Rowland Ogwara <r.ogwara@gmail.com>
2019-09-26 08:12:58 -0500

    updated classifiers

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# flaskdoc
-Flask OpenAPI annotations
+# Introduction
+FlaskDoc is your regular Flask API with builtin support for Swagger documentation. The idea is to have documentation
+as close to code as much as possible.
+
+Requirements
+
+* Python 2.7+, 3.5+
+* Sits on top of Flask
+
+Installation
+
+pip install flaskdoc
+
--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+import pathlib2
 from setuptools import find_packages
 from setuptools import setup
 
 version = "0.0.1"
-reaadme = ""
+
+# The directory containing this file
+HERE = pathlib2.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name="FlaskDoc",
@@ -11,15 +17,28 @@ setup(
     author_email="r.ogwara@gmail.com",
     keywords="swagger, openapi, flask, rest, api",
     description="",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="Apache 2.0",
+    url="https://github.com/kulgan/flaskdoc",
     classifiers=[
-        "Development Status ::: Alpha Stage",
-        "Topics ::: OpenAPI, flask, REST, documentation",
-        "Programming Language :: Python :: 2.7"
+        "Development Status :: 2 - Pre-Alpha",
+        "Framework :: Flask",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Documentation"
     ],
     longdescription="",
-    packages=find_packages("flaskdoc"),
+    packages=find_packages("flaskdoc", exclude="tests"),
+    include_package_data=True,
     package_dir={"": "flaskdoc"},
     install_requires=[
-        "six"
+        "flask",
+        "six",
+        "PyYaml"
     ]
 )
