bd80a1867a44fc1a2a2963602d9e164365676705
Rowland Ogwara <r.ogwara@gmail.com>
2020-07-03 15:11:02 -0500

    rm f-strings

--- src/flaskdoc/swagger/validators.py
+++ src/flaskdoc/swagger/validators.py
@@ -4,4 +4,6 @@ from urllib import parse
 def validate_url(url: str, label: str):
     parsed = parse.urlparse(url)
     if not all([parsed.scheme, parsed.netloc]):
-        raise ValueError(f"{label} entry '{url}' is not a valid url")
+        raise ValueError(
+            "{label} entry '{url}' is not a valid url".format(label=label, url=url)
+        )
